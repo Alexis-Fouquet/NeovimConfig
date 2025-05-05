2087bd3b715de025fb95ffee6817b53444eecc3b
Alexis Fouquet <144385093+Alexis-Fouquet@users.noreply.github.com>
2025-05-05 11:08:59 +0100

    Better start time

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -27,6 +27,7 @@ return {
     },
     {
         'MeanderingProgrammer/render-markdown.nvim',
+        event = 'VeryLazy',
         dependencies = {
             'nvim-treesitter/nvim-treesitter',
             'nvim-tree/nvim-web-devicons'
